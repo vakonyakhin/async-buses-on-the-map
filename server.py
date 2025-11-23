83563f8e13091e3094108d32c6445ac07717c104
Vladimir Koniakhin <vakonyakhin@gmail.com>
2025-11-23 17:35:32 +0300

    Refactor server script from trio to FastAPi

--- server.py
+++ server.py
@@ -1,19 +1,18 @@
-import trio
-from trio_websocket import serve_websocket, ConnectionClosed
+from fastapi import FastAPI, WebSocket
 
 
-async def echo_server(request):
-    ws = await request.accept()
+app = FastAPI()
+
+
+@app.get("/")
+async def get():
+    return {"status": "ok"}
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+
     while True:
-        try:
-            message = await ws.get_message()
-            print(message)
-            #await ws.send_message(f'Your message is - {message}')
-        except ConnectionClosed:
-            break
-
-async def main():
-    # The client connects to '/', so we specify that path here.
-    await serve_websocket(echo_server, '127.0.0.1', 8000, ssl_context=None)
-
-trio.run(main)
+        data = await websocket.receive_text()
+        print(data)
