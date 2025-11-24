6ae6bb6dda20bd2901650ac490a33425bc9c3cf7
Vladimir Koniakhin <vakonyakhin@gmail.com>
2025-11-24 20:37:50 +0300

    Add fake bus emulator script

--- fake_bus.py
+++ fake_bus.py
@@ -1,41 +1,53 @@
-import trio
-from sys import stderr
 import json
-
 import asyncio
-from trio_websocket import open_websocket_url
+
+import anyio
+import zipfile
+from httpx_ws import aconnect_ws
 
 
-async def send_bus(request):
+async def load_routes():
 
-    while True:
-        try:
-            with open("/home/vova/Рабочий стол/python/async-buses-on-the-map/156.json", "r", encoding="utf-8") as f:
-                route_data = json.load(f)
+    with zipfile.ZipFile('routes.zip', 'r') as zip_ref:
+        for file in zip_ref.namelist():
+            if file.endswith('.json'):
+                route = json.loads(zip_ref.read(file))
 
-            route_name = route_data.get("name", "156")
-            coordinates = route_data.get("coordinates", [])
+                yield route
 
+
+async def run_bus():
+    async for route in load_routes():
+        route_name = route.get("name", "not number")
+        coordinates = route.get("coordinates", [])
+        while True:
             for lat, lng in coordinates:
-                message = {
-                    #"msgType": "Buses",
-                    "buses": [
-                        {"busId": "c790сс", "lat": lat, "lng": lng, "route": route_name},
-                    ]
-                }
-                await request.send_message(json.dumps(message))
-                await trio.sleep(1)
-        except Exception:
-            # Handle potential exceptions like file not found or broken connection
-            break
+                    message = {
+                        "msgType": "Buses",
+                        "buses":
+                                [
+                                    {
+                                    "busId": route_name + '-0',
+                                    "lat": lat,
+                                    "lng": lng,
+                                    "route": route_name
+                                    }
+                                ]
+                    }
+            try:
+                async with aconnect_ws('ws://127.0.0.1:8000/ws') as ws:
+
+                    while True:
+                        await ws.send_text(json.dumps(message, ensure_ascii=False))
+                        await asyncio.sleep(1)
+            except OSError as ose:
+                print(f'Connection attempt failed: {ose}')
 
 
 async def main():
-    try:
-        async with open_websocket_url('ws://127.0.0.1:8000/ws') as ws:
-            await send_bus(ws)
-            
-    except OSError as ose:
-        print('Connection attempt failed: %s' % ose, file=stderr)
+    async with anyio.create_task_group() as tg:
+        tg.start_soon(run_bus)
 
-trio.run(main)
+
+if __name__ == "__main__":
+    asyncio.run(main())
