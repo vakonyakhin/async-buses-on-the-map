504b265d15201ef1510afc1fca2b036dfd54e8b1
Vladimir Koniakhin <vakonyakhin@gmail.com>
2025-11-26 21:59:43 +0300

    Implement send coordinates to ws client

--- fake_bus.py
+++ fake_bus.py
@@ -6,6 +6,11 @@ import zipfile
 from httpx_ws import aconnect_ws
 
 
+from fastapi.websockets import WebSocketDisconnect
+
+BUSES = {}
+
+
 async def load_routes():
 
     with zipfile.ZipFile('routes.zip', 'r') as zip_ref:
@@ -16,42 +21,51 @@ async def load_routes():
                 yield route
 
 
-async def run_bus(url, bus_id, route_name, coordinates):
-
+async def talk_to_browser(request):
+    
     while True:
-        for lat, lng in coordinates:
-                message = {
-                    "msgType": "Buses",
-                    "buses":
-                            [
-                                {
-                                "busId": bus_id + '-0',
-                                "lat": lat,
-                                "lng": lng,
-                                "route": route_name
-                                }
-                            ]
-                }
-        try:
-            async with aconnect_ws(url) as ws:
 
-                while True:
-                    await ws.send_text(json.dumps(message, ensure_ascii=False))
-                    await asyncio.sleep(1)
-        except OSError as ose:
-            print(f'Connection attempt failed: {ose}')
+        message = {
+            "msgType": "Buses",
+            "buses": list(BUSES.values())
+        }
+        await request.send_text(json.dumps(message))
+        print(f'message - {message}')
+        await asyncio.sleep(0.1)
+
+
+
+async def run_bus(url, bus_id, route_name, coordinates):
+    try:
+        async with aconnect_ws(url) as ws:
+            while True:
+                for lat, lng in coordinates:
+                    bus_info = {
+                            "busId": bus_id + '-0',
+                            "lat": lat,
+                            "lng": lng,
+                            "route": route_name
+                            }
+
+                    await ws.send_text(json.dumps(bus_info, ensure_ascii=False))
+                    await asyncio.sleep(0.1)
+    except OSError as ose:
+        print(f'Connection attempt failed: {ose}')
+    except (OSError, anyio.EndOfStream) as e:
+        print(f'Connection failed: {e}. Retrying in 5 seconds...')
+    await asyncio.sleep(5)
 
 
 async def main():
 
-    url = 'ws://127.0.0.1:8000/ws'
-    async for route in load_routes():
-        route_name = route.get("name", "not number")
-        bus_id = route_name + '-0'
-        coordinates = route.get("coordinates", [])
-        
+    url = 'ws://127.0.0.1:8000/put_bus'
     async with anyio.create_task_group() as tg:
-        tg.start_soon(run_bus, url, bus_id, route_name, coordinates)
+
+        async for route in load_routes():
+            route_name = route.get("name", "not number")
+            bus_id = route_name
+            coordinates = route.get("coordinates", [])
+            tg.start_soon(run_bus, url, bus_id, route_name, coordinates)
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI, WebSocket
+import json
+import asyncio
+
+import anyio
 
 
+
+BUSES = {}
 app = FastAPI()
 
 
@@ -9,10 +15,56 @@ async def get():
     return {"status": "ok"}
 
 
-@app.websocket("/ws")
+async def talk_to_browser(ws):
+    while True:
+        for bus in BUSES:
+            message = {
+                "msgType": "Buses",
+                "buses":[
+                    {
+                        bus[1]
+                        }
+                ]
+            }
+        await ws.send_text(json.dumps(message))
+        print(f'{message} sent')
+        await asyncio.sleep(1)
+
+
+@app.websocket('/ws')
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    async with anyio.create_task_group() as tg:
+        tg.start_soon(talk_to_browser(websocket))
+    # while True:
+    #     for bus in BUSES.items():
+    #         message = {
+    #             "msgType": "Buses",
+    #             "buses":[
+                    
+    #                     bus[1]
+                        
+    #             ]
+    #         }
+            
+    #         await websocket.send_text(json.dumps(message))
+    #         print(f'{message} sent')
+    #         await asyncio.sleep(1)
+
+    # while True:
+    #     data = await websocket.receive_text()
+    #     print(data)
+
+
+@app.websocket("/put_bus")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
 
     while True:
         data = await websocket.receive_text()
-        print(data)
+        bus_data = json.loads(data)
+        
+        BUSES['busId'] = bus_data
+        print(f'BUSES - {BUSES}')
+    
+        
