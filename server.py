51bc7bcb8a826ca357c7013ec3151060fb87de2e
Vladimir Koniakhin <vakonyakhin@gmail.com>
2025-12-02 15:21:27 +0300

    Refactor code

--- server.py
+++ server.py
@@ -1,59 +1,12 @@
-from fastapi import FastAPI, WebSocket
 import json
-import asyncio
+from fastapi import FastAPI, WebSocket
 
 import anyio
 
+from fake_bus import talk_to_browser, BUSES
 
 
-BUSES = {}
 app = FastAPI()
-
-
-@app.get("/")
-async def get():
-    return {"status": "ok"}
-
-
-async def talk_to_browser(ws):
-    while True:
-        for bus in BUSES:
-            message = {
-                "msgType": "Buses",
-                "buses":[
-                    {
-                        bus[1]
-                        }
-                ]
-            }
-        await ws.send_text(json.dumps(message))
-        print(f'{message} sent')
-        await asyncio.sleep(1)
-
-
-@app.websocket('/ws')
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    async with anyio.create_task_group() as tg:
-        tg.start_soon(talk_to_browser(websocket))
-    # while True:
-    #     for bus in BUSES.items():
-    #         message = {
-    #             "msgType": "Buses",
-    #             "buses":[
-                    
-    #                     bus[1]
-                        
-    #             ]
-    #         }
-            
-    #         await websocket.send_text(json.dumps(message))
-    #         print(f'{message} sent')
-    #         await asyncio.sleep(1)
-
-    # while True:
-    #     data = await websocket.receive_text()
-    #     print(data)
 
 
 @app.websocket("/put_bus")
@@ -63,8 +16,12 @@ async def websocket_endpoint(websocket: WebSocket):
     while True:
         data = await websocket.receive_text()
         bus_data = json.loads(data)
-        
-        BUSES['busId'] = bus_data
-        print(f'BUSES - {BUSES}')
-    
-        
+        BUSES[bus_data['busId']] = bus_data
+        print(len(list(BUSES.values())))
+
+
+@app.websocket('/ws')
+async def browser_websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    async with anyio.create_task_group() as tg:
+        tg.start_soon(talk_to_browser, websocket)
