177da40b682359a9ecbb99491e5103f51bfada43
Vladimir Koniakhin <vakonyakhin@gmail.com>
2025-12-02 16:53:31 +0300

    Implement anyio channels for bus grouping

--- fake_bus.py
+++ fake_bus.py
@@ -3,7 +3,6 @@ import asyncio
 from itertools import cycle, islice
 from random import randint
 
-
 import anyio
 import zipfile
 from httpx_ws import aconnect_ws
@@ -28,6 +27,7 @@ async def talk_to_browser(request):
             "msgType": "Buses",
             "buses": list(BUSES.values())
         }
+        print(list(BUSES.values()))
         await request.send_text(json.dumps(message))
         await asyncio.sleep(1)
 
@@ -36,40 +36,54 @@ def generate_bus_id(route_id, bus_index):
     return f"{route_id}-{bus_index}"
 
 
-async def run_bus(url, bus_id, route_name, coordinates):
-    while True:
-        try:
-            async with aconnect_ws(url) as ws:
-                iterator = islice(cycle(coordinates), randint(1, 50), None)
-                for lat, lng in iterator:
-                    bus_info = {
-                        "busId": bus_id,
-                        "lat": lat,
-                        "lng": lng,
-                        "route": route_name
-                    }
-                    await ws.send_text(json.dumps(bus_info, ensure_ascii=False))
-                    await asyncio.sleep(1)
-        except (OSError, anyio.EndOfStream) as e:
-            print(f'Connection failed for bus {bus_id}: {e}. Retrying in 5 seconds...')
-            await asyncio.sleep(5)
+async def run_bus(url, bus_id, route_name, coordinates, send_channel):
+
+    iterator = islice(cycle(coordinates), randint(1, 50), None)
+    for lat, lng in iterator:
+        bus_info = {
+            "busId": bus_id,
+            "lat": lat,
+            "lng": lng,
+            "route": route_name
+        }
+        await send_channel.send(json.dumps(bus_info, ensure_ascii=False))
+        await asyncio.sleep(1)
 
 
 def generate_bus_id(route_id, bus_index):
     return f"{route_id}-{bus_index}"
 
 
+async def send_updates(server_address, receive_channel):
+
+    try:
+        async with aconnect_ws(server_address) as ws:
+            async for value in receive_channel:
+                await ws.send_text(value)
+    except (OSError, anyio.EndOfStream):
+        print(f'Connection failed. Retrying in 5 seconds...')
+        await asyncio.sleep(5)
+
+
 async def main():
 
     url = 'ws://127.0.0.1:8000/put_bus'
-    async with anyio.create_task_group() as tg:
 
+    async with anyio.create_task_group() as tg:
+        
         async for route in load_routes():
             route_name = route.get("name", "not number")
             coordinates = route.get("coordinates", [])
-            for _ in range(10):
+            # Создаем один канал для одного маршрута
+            send_channel, receive_channel = anyio.create_memory_object_stream(0)
+            
+            # Запускаем одну задачу для отправки данных на сервер для этого маршрута
+            tg.start_soon(send_updates, url, receive_channel)
+
+            # Запускаем несколько автобусов, которые пишут в один и тот же канал
+            for _ in range(3):
                 bus_id = generate_bus_id(route_name, randint(1, 1000))
-                tg.start_soon(run_bus, url, bus_id, route_name, coordinates)
+                tg.start_soon(run_bus, url, bus_id, route_name, coordinates, send_channel)
 
 
 if __name__ == "__main__":
